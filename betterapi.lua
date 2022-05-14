bb047e195b1a71a98783c65ecbb606361b1f0b50
Nonamegone <65653743+Nonamegone@users.noreply.github.com>
2022-05-14 14:37:39 +0300

    Forgot lib

--- betterapi.lua
+++ betterapi.lua
@@ -1,33 +1,10 @@
 ffi = require("ffi")
 
 ffi.cdef[[
-    int MessageBoxA(void *w, const char *txt, const char *cap, int type);
-    bool CreateDirectoryA(const char* lpPathName, void* lpSecurityAttributes);
-    int exit(int arg);
 
-    void* __stdcall URLDownloadToFileA(void* LPUNKNOWN, const char* LPCSTR, const char* LPCSTR2, int a, int LPBINDSTATUSCALLBACK);        
-    void* __stdcall ShellExecuteA(void* hwnd, const char* op, const char* file, const char* params, const char* dir, int show_cmd);
+    typedef unsigned long DWORD, *PDWORD, *LPDWORD;  
+    typedef unsigned char byte;
 
-    int AddFontResourceA(const char* unnamedParam1);
-
-    bool DeleteUrlCacheEntryA(const char* lpszUrlName);
-
-    typedef struct tagPOINT {
-        long x;
-        long y;
-    } POINT;
-
-    bool GetCursorPos(
-        POINT* lpPoint
-    );
-
-    short GetAsyncKeyState(
-        int vKey
-    );
-
-    void* GetProcAddress(void* hModule, const char* lpProcName);
-    void* GetModuleHandleA(const char* lpModuleName);
- 
     typedef struct {
         uint8_t r;
         uint8_t g;
@@ -35,22 +12,26 @@ ffi.cdef[[
         uint8_t a;
     } color_struct_t;
 
-    typedef void (*console_color_print)(const color_struct_t&, const char*, ...);
+    typedef struct {
+        float x,y;
+    } vec2_t;
 
-    typedef unsigned long DWORD, *PDWORD, *LPDWORD;  
+    typedef struct {
+        vec2_t m_Position;
+        vec2_t m_TexCoord;
+    } Vertex_t;
 
-    typedef void (__cdecl* chat_printf)(void*, int, int, const char*, ...);
-
-    typedef int(__fastcall* clantag_t)(const char*, const char*);
-
-    // HITBOXPOS START
-
-    typedef unsigned char byte;
+    typedef struct tagPOINT {
+        long x;
+        long y;
+    } POINT;
 
     typedef struct
     {
         float x,y,z;
-    } Vector;
+    } vec3_t;
+
+    // HITBOXPOS STRUCTS START
 
     typedef struct
     {
@@ -59,12 +40,12 @@ ffi.cdef[[
         long    checksum;               //0x0008
         char    szName[64];             //0x000C
         int length;                 //0x004C
-        Vector  vecEyePos;              //0x0050
-        Vector  vecIllumPos;            //0x005C
-        Vector  vecHullMin;             //0x0068
-        Vector  vecHullMax;             //0x0074
-        Vector  vecBBMin;               //0x0080
-        Vector  vecBBMax;               //0x008C
+        vec3_t  vecEyePos;              //0x0050
+        vec3_t  vecIllumPos;            //0x005C
+        vec3_t  vecHullMin;             //0x0068
+        vec3_t  vecHullMax;             //0x0074
+        vec3_t  vecBBMin;               //0x0080
+        vec3_t  vecBBMax;               //0x008C
         int pad[5];
         int numhitboxsets;          //0x00AC
         int hitboxsetindex;         //0x00B0
@@ -78,8 +59,8 @@ ffi.cdef[[
         int nServerCount;           //0x010C
         int type;                   //0x0110
         int flags;                  //0x0114
-        Vector  vecMins;                //0x0118
-        Vector  vecMaxs;                //0x0124
+        vec3_t  vecMins;                //0x0118
+        vec3_t  vecMaxs;                //0x0124
         float   radius;                 //0x0130
         char    pad[28];              //0x0134
     } model_t;
@@ -88,10 +69,10 @@ ffi.cdef[[
     {
         int     m_bone;                 // 0x0000
         int     m_group;                // 0x0004
-        Vector  m_mins;                 // 0x0008
-        Vector  m_maxs;                 // 0x0014
+        vec3_t  m_mins;                 // 0x0008
+        vec3_t  m_maxs;                 // 0x0014
         int     m_name_id;                // 0x0020
-        Vector  m_angle;                // 0x0024
+        vec3_t  m_angle;                // 0x0024
         float   m_radius;               // 0x0030
         int        pad2[4];
     } mstudiobbox_t;
@@ -108,9 +89,50 @@ ffi.cdef[[
         float m_flMatVal[3][4];
     } matrix3x4_t;
 
+    // HITBOXPOS STRUCTS END
+
+    unsigned int mbstowcs(wchar_t* w, const char* s, unsigned int c);
+
+    bool GetCursorPos(
+        POINT* lpPoint
+    );
+
+    short GetAsyncKeyState(
+        int vKey
+    );
+    int MessageBoxA(void *w, const char *txt, const char *cap, int type);
+    bool CreateDirectoryA(const char* lpPathName, void* lpSecurityAttributes);
+    int exit(int arg);
+
+    void* __stdcall URLDownloadToFileA(void* LPUNKNOWN, const char* LPCSTR, const char* LPCSTR2, int a, int LPBINDSTATUSCALLBACK);        
+    void* __stdcall ShellExecuteA(void* hwnd, const char* op, const char* file, const char* params, const char* dir, int show_cmd);
+
+    int AddFontResourceA(const char* unnamedParam1);
+
+    bool DeleteUrlCacheEntryA(const char* lpszUrlName);
+
+    bool GetCursorPos(
+        POINT* lpPoint
+    );
+
+    short GetAsyncKeyState(
+        int vKey
+    );
+
+    void* GetProcAddress(void* hModule, const char* lpProcName);
+    void* GetModuleHandleA(const char* lpModuleName);
+
+    typedef void (*console_color_print)(const color_struct_t&, const char*, ...);
+
+    typedef void (__cdecl* chat_printf)(void*, int, int, const char*, ...);
+
+    typedef int(__fastcall* clantag_t)(const char*, const char*);
+
+    // HITBOXPOS FUNCS START
+
     typedef bool(__fastcall* cbaseanim_setupbones)(matrix3x4_t *pBoneToWorldOut, int nMaxBones, int boneMask, float currentTime);
 
-    // HITBOXPOS END
+    // HITBOXPOS FUNCS END
 
     // PANORAMA START
 
@@ -242,6 +264,13 @@ ffi.cdef[[
 ]]
 
 --- MEMORY CDEF END
+
+local vmt_bind = function(vmt_table, func, index)
+    local result = ffi.cast(ffi.typeof(func), vmt_table[0][index])
+    return function(...)
+        return result(vmt_table, ...)
+    end
+end
 
 local function panoramalib()
     local interface_ptr = ffi.typeof("void***")
@@ -458,6 +487,83 @@ local function memorylib()
     return proc
 end
 
+local new_vert = ffi.typeof('Vertex_t[?]')
+local new_vec2 = ffi.typeof('vec2_t')
+local zerovec = new_vec2()
+zerovec.x, zerovec.y = 0,0
+local charbuffer = ffi.typeof("char[?]")
+local new_intptr = ffi.typeof("int[1]")
+local new_boolptr = ffi.typeof('bool[1]')
+local new_int = ffi.typeof('int')
+local new_widebuffer = ffi.typeof("wchar_t[?]")
+
+vec2 = {}
+vec2.new = function(x,y)
+    return ffi.new(new_vec2, {x,y})
+end
+
+local native_Surface, Cast_m_bClippingEnabled, clipCache, PrintText, font_cache, ConvertAnsiToUnicode = (function()
+    local RawLocalize = utils.create_interface('localize.dll', 'Localize_001')
+    local Localize    = ffi.cast(ffi.typeof('void***'), RawLocalize)
+
+    local FindSafe = vmt_bind(Localize, 'wchar_t*(__thiscall*)(void*, const char*)', 12)
+    local ConvertAnsiToUnicode = vmt_bind(Localize, 'int(__thiscall*)(void*, const char*, wchar_t*, int)', 15)
+    local ConvertUnicodeToAnsi = vmt_bind(Localize, 'int(__thiscall*)(void*, wchar_t*, char*, int)', 16)
+
+    local VGUI_Surface031 = utils.create_interface("vguimatsurface.dll", "VGUI_Surface031")
+    local g_VGuiSurface = ffi.cast(ffi.typeof("void***"), VGUI_Surface031)
+    local native_Surface = {}
+    native_Surface.DrawSetColor = vmt_bind(g_VGuiSurface, "void(__thiscall*)(void*, int, int, int, int)", 15)
+    native_Surface.DrawTexturedRect = vmt_bind(g_VGuiSurface, "void(__thiscall*)(void*, int, int, int, int)", 41)
+    native_Surface.DrawSetTextureRGBA = vmt_bind(g_VGuiSurface, "void(__thiscall*)(void*, int, const unsigned char*, int, int)", 37)
+    native_Surface.DrawSetTexture = vmt_bind(g_VGuiSurface, "void(__thiscall*)(void*, int)", 38)
+    native_Surface.DrawFilledRectFade = vmt_bind(g_VGuiSurface, "void(__thiscall*)(void*, int, int, int, int, unsigned int, unsigned int, bool)", 123)
+    native_Surface.DrawFilledRect =  vmt_bind(g_VGuiSurface, 'void(__thiscall*)(void*, int, int, int, int)', 16)
+    native_Surface.DrawOutlinedRect = vmt_bind(g_VGuiSurface, 'void(__thiscall*)(void*, int, int, int, int)', 18)
+    native_Surface.CreateNewTextureID = vmt_bind(g_VGuiSurface, "int(__thiscall*)(void*, bool)", 43)
+    native_Surface.IsTextureIDValid = vmt_bind(g_VGuiSurface, "bool(__thiscall*)(void*, int)", 42)
+    native_Surface.FontCreate = vmt_bind(g_VGuiSurface, 'unsigned long(__thiscall*)(void*)', 71)
+    native_Surface.SetFontGlyphSet = vmt_bind(g_VGuiSurface, 'void(__thiscall*)(void*, unsigned long, const char*, int, int, int, int, unsigned long, int, int)', 72)
+    native_Surface.GetTextSize = vmt_bind(g_VGuiSurface, 'void(__thiscall*)(void*, unsigned long, const wchar_t*, int&, int&)', 79)
+    native_Surface.DrawSetTextColor = vmt_bind(g_VGuiSurface, 'void(__thiscall*)(void*, int, int, int, int)', 25)
+    native_Surface.DrawSetTextFont = vmt_bind(g_VGuiSurface, 'void(__thiscall*)(void*, unsigned long)', 23)
+    native_Surface.DrawSetTextPos = vmt_bind(g_VGuiSurface, 'void(__thiscall*)(void*, int, int)', 26)
+    native_Surface.DrawPrintText = vmt_bind(g_VGuiSurface, 'void(__thiscall*)(void*, const wchar_t*, int, int)', 28)
+    native_Surface.DrawLine = vmt_bind(g_VGuiSurface, 'void(__thiscall*)(void*, int, int, int, int)', 19)
+    native_Surface.DrawTexturedPolygon = vmt_bind(g_VGuiSurface, "void(__thiscall*)(void*, int, Vertex_t*, bool)", 106)
+    native_Surface.DrawTexturedPolyLine = vmt_bind(g_VGuiSurface, "void(__thiscall*)(void*, const Vertex_t*, int)", 104)
+    native_Surface.LimitDrawingArea = vmt_bind(g_VGuiSurface, "void(__thiscall*)(void*, int, int, int, int)", 147)
+    native_Surface.GetDrawingArea = vmt_bind(g_VGuiSurface, "void(__thiscall*)(void*, int&, int&, int&, int&)", 146)
+
+    local PrintText = function(text, localized)
+        local size = 1024.0
+        if localized then
+            local char_buffer = charbuffer(size)
+            ConvertUnicodeToAnsi(text, char_buffer, size)
+        
+            return native_Surface.DrawPrintText(text, #ffi.string(char_buffer), 0)
+        else
+            local wide_buffer = new_widebuffer(size)
+        
+            ConvertAnsiToUnicode(text, wide_buffer, size)
+            return native_Surface.DrawPrintText(wide_buffer, #text, 0)
+        end
+    end
+
+    local font_cache = {}
+
+    local Cast_m_bClippingEnabled = ffi.cast('int*', VGUI_Surface031 + 0x280)
+
+    local clipCache = {
+        x = new_intptr(),
+        y = new_intptr(),
+        x1 = new_intptr(),
+        y1 = new_intptr(),
+    }
+
+
+    return native_Surface, Cast_m_bClippingEnabled, clipCache, PrintText, font_cache, ConvertAnsiToUnicode
+end)()
 local urlmon = ffi.load 'UrlMon'
 local wininet = ffi.load 'WinInet'
 local gdi = ffi.load 'Gdi32'
@@ -465,10 +571,9 @@ local gdi = ffi.load 'Gdi32'
 local g_VGuiSurface = ffi.cast(ffi.typeof("void***"), utils.create_interface("vguimatsurface.dll", "VGUI_Surface031"))
 local native_Surface_DrawSetColor = ffi.cast(ffi.typeof("void(__thiscall*)(void*, int, int, int, int)"), g_VGuiSurface[0][15])
 local native_Surface_DrawFilledRectFade = ffi.cast(ffi.typeof("void(__thiscall*)(void*, int, int, int, int, unsigned int, unsigned int, bool)"), g_VGuiSurface[0][123])
-
 local ffi_helpers = {
-    color_print_fn = ffi.cast("console_color_print", ffi.C.GetProcAddress(ffi.C.GetModuleHandleA("tier0.dll"), "?ConColorMsg@@YAXABVColor@@PBDZZ")),
-    color_print = function(self, text, color)
+color_print_fn = ffi.cast("console_color_print", ffi.C.GetProcAddress(ffi.C.GetModuleHandleA("tier0.dll"), "?ConColorMsg@@YAXABVColor@@PBDZZ")),
+color_print = function(self, text, color)
         local col = ffi.new("color_struct_t")
 
         col.r = color:r()
@@ -479,6 +584,23 @@ local ffi_helpers = {
         self.color_print_fn(col, text)
     end
 }
+
+EFontFlags = ffi.typeof([[
+    enum {
+	    NONE,
+	    ITALIC			= 0x001,
+	    UNDERLINE		= 0x002,
+	    STRIKEOUT		= 0x004,
+	    SYMBOL			= 0x008,
+	    ANTIALIAS		= 0x010,
+	    GAUSSIANBLUR		= 0x020,
+	    ROTARY			= 0x040,
+	    DROPSHADOW		= 0x080,
+	    ADDITIVE		= 0x100,
+	    OUTLINE		= 0x200,
+	    CUSTOM			= 0x400,
+    }
+]])
 
 local FindHudElement = function(name)
     local pThis = ffi.cast(ffi.typeof("DWORD**"), utils.find_signature("client.dll", "B9 ? ? ? ? 68 ? ? ? ? E8 ? ? ? ? 89 46 24") + 1)[0]
@@ -616,16 +738,143 @@ end
 
 --- DRAW START ---
 
-draw.gradientrect = function(x, y, w, h, color, color2, horizontal)
-    local r0, g0, b0, a0 = color:r(), color:g(), color:b(), color:a()
-    local r1, g1, b1, a1 = color2:r(), color2:g(), color2:b(), color2:a()
-
-    native_Surface_DrawSetColor(g_VGuiSurface,r0, g0, b0, a0)
-    native_Surface_DrawFilledRectFade(g_VGuiSurface,x, y, x + w, y + h, 255, 0, horizontal)
-    native_Surface_DrawSetColor(g_VGuiSurface,r1, g1, b1, a1)
-    return native_Surface_DrawFilledRectFade(g_VGuiSurface,x, y, x + w, y + h, 0, 255, horizontal)
+draw.set_clip = function (x,y,w,h)
+    Cast_m_bClippingEnabled[0] = true
+    native_Surface.GetDrawingArea(clipCache.x, clipCache.y, clipCache.x1, clipCache.y1)
+    native_Surface.LimitDrawingArea(x,y,x + w,y + h)
 end
 
+draw.end_clip = function ()
+    native_Surface.LimitDrawingArea(clipCache.x[0], clipCache.y[0], clipCache.x1[0], clipCache.y1[0])
+    Cast_m_bClippingEnabled[0] = false
+end
+
+draw.line = function(x0, y0, x1, y1, r, g, b, a)
+	native_Surface.DrawSetColor(r, g, b, a)
+	return native_Surface.DrawLine(x0, y0, x1, y1)
+end
+
+draw.rect = function(x, y, w, h, r, g, b, a)
+	native_Surface.DrawSetColor(r, g, b, a)
+	return native_Surface.DrawOutlinedRect(x, y, x + w, y + h)
+end
+
+draw.filled_rect = function(x, y, w, h, r, g, b, a)
+    native_Surface.DrawSetColor(r, g, b, a)
+    return native_Surface.DrawFilledRect(x, y, x + w, y + h)
+end
+
+draw.gradientrect = function(x, y, w, h, r0, g0, b0, a0, r1, g1, b1, a1, horizontal)
+    native_Surface.DrawSetColor(r0, g0, b0, a0)
+    native_Surface.DrawFilledRectFade(x, y, x + w, y + h, 255, 0, horizontal)
+    native_Surface.DrawSetColor(r1, g1, b1, a1)
+    return native_Surface.DrawFilledRectFade(x, y, x + w, y + h, 0, 255, horizontal)
+end
+
+draw.polygon = function(vertices, clipvertices, r, g, b, a)
+    local a = a or 255
+    local numvert = #vertices
+    local buf = new_vert(numvert)
+    local i = 0
+    for k,v in pairs(vertices) do
+        local vec = new_vec2()
+        vec.x, vec.y = v.x, v.y
+        buf[i].m_Position = vec
+        buf[i].m_TexCoord = zerovec
+        i = i + 1
+    end
+    native_Surface.DrawSetColor(r, g, b, a)
+    native_Surface.DrawSetTexture(-1)
+    native_Surface.DrawTexturedPolygon(numvert, buf, clipvertices)
+end
+
+draw.polyline = function(vertices, r, g, b, a)
+    local a = a or 255
+    local numvert = #vertices
+    local buf = new_vert(numvert)
+    local i = 0
+    for k,v in pairs(vertices) do
+        local vec = new_vec2()
+        vec.x, vec.y = v.x, v.y
+        buf[i].m_Position = vec
+        buf[i].m_TexCoord = zerovec
+        i = i + 1
+    end
+    native_Surface.DrawSetColor(r, g, b, a)
+    native_Surface.DrawSetTexture(-1)
+    native_Surface.DrawTexturedPolyLine(buf, numvert)
+end
+
+draw.init_rgba = function(data, w, h)
+    local returntbl = {}
+    local tid = native_Surface.CreateNewTextureID(true)
+    if not tid or not native_Surface.IsTextureIDValid(tid) or not data then return end
+    native_Surface.DrawSetTextureRGBA(tid, data, w, h)
+    returntbl.tid = tid
+    returntbl.data = data
+    return returntbl
+end
+
+draw.rgba = function(datatbl, x, y, w, h, alpha)
+    local tid = datatbl.tid
+    native_Surface.DrawSetColor(255,255,255,alpha)
+    native_Surface.DrawSetTexture(tid)
+    --native_Surface.DrawSetTextureRGBA(g_VGuiSurface, tid, data, w, h)
+    return native_Surface.DrawTexturedRect(x, y, x + w, y + h)
+end
+
+draw.init_font = function(name, h, weight, blur, flags)
+	local flags_i = 0
+	local t = type(flags)
+	if t == "number" then
+		flags_i = flags
+	elseif t == "table" then
+		for i = 1, #flags do
+			flags_i = flags_i + flags[i]
+		end
+	else
+		flags_i = 0x0
+	end
+
+	local cache_key = string.format("%s\0%d\0%d\0%d", name, h, weight, flags_i)
+	if font_cache[cache_key] == nil then
+		font_cache[cache_key] = native_Surface.FontCreate()
+		native_Surface.SetFontGlyphSet(font_cache[cache_key], name, h, weight, blur, 0, flags_i, 0, 0)
+	end
+
+	return font_cache[cache_key]
+end
+
+draw.get_textsize = function(font, text)
+	local wide_buffer = new_widebuffer(1024)
+	local w_ptr = new_intptr()
+	local h_ptr = new_intptr()
+
+	ConvertAnsiToUnicode(text, wide_buffer, 1024)
+	native_Surface.GetTextSize(font, wide_buffer, w_ptr, h_ptr)
+    
+	return vec2.new(tonumber(w_ptr[0]), tonumber(h_ptr[0]))
+end
+
+draw.text = function(font, x, y, r, g, b, a, text)
+    native_Surface.DrawSetTextPos(x, y)
+	native_Surface.DrawSetTextFont(font)
+	native_Surface.DrawSetTextColor(r, g, b, a)
+    return PrintText(text, false)
+end
+
+draw.text_centered = function(font, x, y, r, g, b, a, text, centered_x, centered_y)
+	local text_size = draw.get_textsize(font, text)
+	local x,y = x,y
+	if centered_x then
+		x = x - text_size.x / 2
+	end
+	if centered_y then
+		y = y - text_size.y / 2
+	end
+    return draw.text(font,x,y,r,g,b,a,text)
+end
+--local native_Surface_DrawSetTextureRGBA = ffi.cast(ffi.typeof("void(__thiscall*)(void*, int, const unsigned char*, int, int)"), g_VGuiSurface[0][37])
 
 --- DRAW END ---
 
@@ -660,7 +909,7 @@ entlist.get_players = function(num) -- 0 - enemies only, 1 - teammates only, 2 -
     return players
 end
 
-entlist.find_by_class = function(class)
+entlist.find_by_class = function(class) -- Example: "CBasePlayer"
     for i=64, GetHighestEntityIndex(IClientEntityList) do
         local ent = entitylist.get_player_by_index(i)
         
